0003dc62966050d0674d4444a4a661c663d84dcb
Josema <jm3develop@gmail.com>
2024-11-17 19:12:18 +0100

    added prettier tailwind plugin formatter

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,5 @@
-<div {{$attributes->class(['rounded-md border border-slate-300 bg-white p-4 shadow-sm'])}} >
-    {{$slot}}
-</div>
+<article
+    {{ $attributes->class(["rounded-md border border-slate-300 bg-white p-4 shadow-sm"]) }}
+>
+    {{ $slot }}
+</article>
--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,5 +1,10 @@
 <x-layout>
-    @foreach ($jobs as $job )
-    <x-card class="mb-4">{{$job->title}}</x-card>
+    @foreach ($jobs as $job)
+        <x-card class="mb-4">
+            <div class="mb-4 flex items-center justify-between">
+                <h2 class="text-lg font-medium">{{ $job->title }}</h2>
+                <div>Salary:</div>
+            </div>
+        </x-card>
     @endforeach
 </x-layout>
