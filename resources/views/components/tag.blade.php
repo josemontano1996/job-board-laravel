4da1fdb40799910cd2cbc4cb5c1e9d89bf9d6db3
Josema <jm3develop@gmail.com>
2024-11-17 19:35:57 +0100

    added card component

---
+++ resources/views/components/tag.blade.php
@@ -0,0 +1,5 @@
+<span
+    {{ $attributes->class(["rounded-md border px-2 py-1"]) }}
+>
+    {{ $slot }}
+</span>
--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -3,8 +3,28 @@
         <x-card class="mb-4">
             <div class="mb-4 flex items-center justify-between">
                 <h2 class="text-lg font-medium">{{ $job->title }}</h2>
-                <div>Salary:</div>
+                <div class="text-slate-500">
+                    $ {{ number_format($job->salary) }}
+                </div>
             </div>
+            <div
+                class="mb-4 flex items-center justify-between text-sm text-slate-500"
+            >
+                <div class="flex items-center space-x-4">
+                    <p>Company name</p>
+                    <p>{{ $job->location }}</p>
+                </div>
+                <div class="flex items-center space-x-2 text-xs">
+                    <x-tag>{{ Str::ucfirst($job->experience) }}</x-tag>
+                    <x-tag>
+                        {{ Str::ucfirst($job->category) }}
+                    </x-tag>
+                </div>
+            </div>
+            {{-- Escaping the html for the description at the samke time as parsing the br --}}
+            <p class="text-sm text-slate-500">
+                {!! nl2br(e($job->description)) !!}
+            </p>
         </x-card>
     @endforeach
 </x-layout>
