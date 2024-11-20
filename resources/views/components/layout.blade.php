f6994bbcbc91c600737166c2a914c51f9632d327
Josema <jm3develop@gmail.com>
2024-11-20 21:59:38 +0100

    added log out

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,7 +15,31 @@
     <body
         class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-indigo-100 from-10% via-sky-100 via-30% to-emerald-100 to-90%"
     >
-        {{ auth()->user()->name ?? "Guest" }}
+        <nav class="mb-8 flex items-center justify-between font-medium">
+            <ul class="flex space-x-2 text-lg">
+                <li><a href="{{ route("jobs.index") }}">Home</a></li>
+            </ul>
+            <ul class="flex items-center space-x-2 text-sm">
+                @auth
+                    <li>
+                        {{ auth()->user()->name }}
+                    </li>
+                    <span>|</span>
+                    <li>
+                        <form
+                            action="{{ route("auth.destroy") }}"
+                            method="POST"
+                        >
+                            @csrf
+                            @method("DELETE")
+                            <button>Log out</button>
+                        </form>
+                    </li>
+                @else
+                    <li><a href="{{ route("auth.create") }}">Sign in</a></li>
+                @endauth
+            </ul>
+        </nav>
         {{ $slot }}
     </body>
 </html>
