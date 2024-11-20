f208bbe24454bdb4c3c8a5527ef02814737445f0
Josema <jm3develop@gmail.com>
2024-11-20 19:02:33 +0100

    added the new seeder and seeded db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Employer;
 use App\Models\JobOffer;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -14,8 +15,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        JobOffer::factory(100)->create();
-        // User::factory(10)->create();
+        User::factory(300)->create();
+
+        $users = User::all()->shuffle();
+
+        for ($i = 0; $i < 20; $i++) {
+            Employer::factory()->create([
+                'user_id' => $users->pop()->id
+            ]);
+        }
+
+
+        $employers = Employer::all();
+
+        for ($i = 0; $i < 200; $i++) {
+            JobOffer::factory()->create([
+                'employer_id' => $employers->random()->id
+            ]);
+        }
 
     }
 }
